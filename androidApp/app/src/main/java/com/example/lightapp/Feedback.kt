31ccf10869bc256be1fe7be262f2a6b7cbd6eb45
Donggeon <ehdrjs6831@naver.com>
2024-09-10 22:40:55 +0900

    Delete all files

--- androidApp/app/src/main/java/com/example/lightapp/Feedback.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lightapp
-
-data class Feedback(
-    val user: String,
-    val feedback: String
-)
--- androidApp/app/src/main/java/com/example/lightapp/FeedbackResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lightapp
-
-data class FeedbackResponse(
-    val status: String,
-    val message: String
-)
